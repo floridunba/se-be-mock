8bc686d193448c64886f48e84bb05ff4206db492
floridunba <suncytk@gmail.com>
2026-04-15 23:02:32 +0700

    Improve getReviews(filter by camp, user) and route permission

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,10 +2,30 @@ const Review = require('../models/Review');
 const Campground = require('../models/Campground');
 
 exports.getReviews = async (req, res, next) => {
-  try {
-    const reviews = await Review.find({
+  let query;
+  if (req.params.campgroundId) {
+    console.log(req.params.campgroundId)
+    if(req.user && req.user.role === "user"){
+      query = Review.find({
+        user: req.user.id,
         campground: req.params.campgroundId
-      }); // get campground reviews
+      }).populate("user", "_id name");
+    }
+    else {
+      query = Review.find({
+        campground: req.params.campgroundId
+      }).populate("user", "_id name");
+    }
+  }
+  else {
+    if(req.user.role !== 'admin'){
+        query = Review.find({user:req.user.id});
+    } else {
+      query = Review.find({}).populate("user", "_id name");
+    }
+  }
+  try {
+    const reviews = await query;
 
     res.status(200).json({
       success: true,
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,8 @@ const { getReviews } = require('../controllers/reviews');
 const { protect, authorize } = require('../middleware/auth');
 const router = express.Router({ mergeParams: true});
 
-router.route('/').get(getReviews)
+router.route('/').get(getReviews);
+router.route('/user').get(protect, authorize('admin', 'user'), getReviews);
+router.route('/:campgroundId').get(protect, authorize('admin', 'user'), getReviews);
 
 module.exports=router
